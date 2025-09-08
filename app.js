024d40c51f5d7e76a77a0f97e24a6b360dba8a95
phiphipiupiu <128450965+phiyixian@users.noreply.github.com>
2025-09-08 10:51:08 +0800

    express query and api calling

--- app.js
+++ app.js
@@ -22,6 +22,51 @@ app.get('/api/products', (req, res) => {
     res.json(newProducts);
 })
 
+app.get('/api/products/:productID', (req, res) => {
+    // console.log(req);
+    // console.log(req.params);
+    const {productID} = req.params;
+
+    const singleProduct = products.find(
+        (product) => product.id === Number(productID)
+    )
+    if(!singleProduct) {
+        return res.status(404).send('Product does not exist.');
+    }
+    console.log(singleProduct);
+    res.json(singleProduct);
+})
+
+app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
+    console.log(req.params);
+    res.send('hello world');
+})
+
+app.get('/api/v1/query', (req,res) => {
+    // console.log(req.query);
+    const { search,limit } = req.query;
+    let sortedProducts = [...products];
+
+    if(search) {
+        sortedProducts = sortedProducts.filter((product) => {
+            return product.name.startsWith(search);
+        })
+    }
+
+    if(limit) {
+        sortedProducts = sortedProducts.slice(0, Number(limit));
+    }
+
+    if(sortedProducts.length < 1) {
+        // This will cause error because cannot send two responses for same request at the same time
+        // res.status(200).send('No products matched your search');
+        return res.status(200).json({success: true, data: []});
+    }
+
+    res.status(200).json(sortedProducts);
+    res.send("hello query");
+})
+
 app.listen(5000, () => {
     console.log('Server is listening on port 5000...');
 })
